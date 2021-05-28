115a164225c24dd5667925eedede4c61eff4eb51
curtgithub <curtgithub@gmail.com>
2021-05-28 12:06:50 -0400

    First commit.

---
+++ src/swingutils/DirectoryBrowser.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingutils;
+
+import javax.swing.JFileChooser;
+
+/**
+ *
+ * @author owner
+ */
+public class DirectoryBrowser extends JFileChooser {
+
+   public DirectoryBrowser(String currentDirectoryPath) {
+      super(currentDirectoryPath);
+   }
+    
+
+}
